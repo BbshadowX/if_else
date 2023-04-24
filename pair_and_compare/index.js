918e77e0241572d1d71d867f30fa0ab3cc8530e7
Bryan Crawford <bryancrawford14@gmail.com>
2023-04-24 16:10:25 -0400

    Created a if else statment for pair and compare index.js

--- pair_and_compare/index.js
+++ pair_and_compare/index.js
@@ -1,3 +1,12 @@
+const param1A = input1
+const param1B = input2
+const param2A = input3
+const param2B = input4
 // define 4 variables param1A, param1B, param2A, and param2B
-// if atlease one of the pair is truthy print true
-// if none of the pair is truthy print false
+if (param1A == param1B || param2A == param2B) {
+    console.log (true)
+    // if atlease one of the pair is truthy print true
+    } else {
+    console.log (false)
+    // if nne of the pair is truthy print false
+    }
